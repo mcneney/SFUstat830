072e048b0a9bb66f2b42bcbe8bbac0d32ee7cb7d
mcneney <mcneney@sfu.ca>
2016-10-17 17:07:38 -0700

    first draft of code for ECDFs

--- Lectures/chapter7.R
+++ Lectures/chapter7.R
@@ -1,4 +1,30 @@
-dat = scan("http://www.stat.cmu.edu/~larry/all-of-statistics/=data/nerve.dat")
 
-ee = ecdf(dat)
+# Plot ECDF of example from notes
+ee <- ecdf(c(5,3,7))
 plot(ee)
+
+# Now plot ECDF for nerve data from text
+x <- scan("http://www.stat.cmu.edu/~larry/all-of-statistics/=data/nerve.dat")
+ee <- ecdf(x)
+plot(ee)
+
+ee(.5)
+abline(v=.5)
+abline(h=ee(.5))
+
+ee(.6) - ee(.4)
+
+# Draw our own and add pointwise CIs. Adapted from code on author's website:
+#    http://www.stat.cmu.edu/~larry/all-of-statistics/=Rprograms/edf.r
+ox <- sort(x) # ordered x
+n <- length(x)
+Fhatn <- (1:n)/n
+plot(odat,Fn,type="l")
+alpha <- 0.05
+eps <- sqrt(log(2/alpha)/(2*n))
+print(eps)
+upper <- pmin(Fhatn + eps,1)
+lower <- pmax(Fhatn - eps,0)
+lines(ox,upper,type="s",lwd=2,col=2,lty=2)
+lines(ox,lower,type="s",lwd=2,col=2,lty=2)
+rug(x,ticksize=.025)
